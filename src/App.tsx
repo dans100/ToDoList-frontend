340c667a174bce37ba79f8570da7d5382a24a543
Daniel Sulkowski <dansulk@gmail.com>
2023-01-23 16:09:26 +0100

    Code Review

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {TodoListView} from "./views/TodoListView";
 import {NotFoundView} from "./views/NotFoundView";
+import {Login} from "./components/Login/Login";
+
+
 
 export const App = () => {
-  return (
-    <Routes>
-      <Route path='/list' element={<TodoListView/>}/>
-        <Route path='/*' element={<NotFoundView/>}/>
-    </Routes>
-  );
+
+    return (
+
+        <Routes>
+            <Route path='/list' element={<TodoListView/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/*' element={<NotFoundView/>}/>
+        </Routes>
+    );
 }
 
 
