1a9b6e1e66904cecc59ebb04829e59d85c477e93
Daniel Sulkowski <dansulk@gmail.com>
2023-03-01 21:46:49 +0100

    Added Footer and some packages

--- src/views/TodoListView.tsx
+++ src/views/TodoListView.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {TodoList} from "../components/TodoList/TodoList";
+import {Footer} from "../components/Layout/Footer";
+
 
 export const TodoListView = () => {
     return (
-        <TodoList/>
+        <>
+            <TodoList/>
+            <Footer/>
+        </>
     )
 }
