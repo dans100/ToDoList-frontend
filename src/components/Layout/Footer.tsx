48ba92b6baafc44254cf8d687bccde8876cbf1d4
Daniel Sulkowski <dansulk@gmail.com>
2023-03-01 21:42:29 +0100

    Added Footer with styles

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import {LinkedIn} from "@mui/icons-material";
+
+
+export const Footer = () => {
+
+    return (
+        <footer className='footer-app'>
+            <p>&copy; 2023 dans100
+                <span>
+                        <a href="https://github.com/dans100" target="_blank"><GitHubIcon/></a>
+                    </span>
+                <span>
+                        <a href="https://www.linkedin.com/in/daniel-su%C5%82kowski-35b0ab257/"
+                           target="_blank"><LinkedIn/></a>
+                    </span>
+            </p>
+        </footer>
+    )
+}
